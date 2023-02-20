878df54810f82b7f575d8e6b97704f175c134df9
Cesar Gerardo Guzman Lopez <47696662+CesarGuzmanLopez@users.noreply.github.com>
2023-02-20 09:44:28 -0600

    new tardis

--- tardis.sh
+++ tardis.sh
@@ -20,30 +20,35 @@ BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 # Obtener el ancho de la pantalla
 cols=$(tput cols)
-cols=$((cols+10))
+cols=$((cols+12))
+
+
+
 
 # Salida del sistema con emojis y formato
-echo -e "。★  ˚ •    -   ˚ •。★˚˛˚     ${GREEN}${BOLD}👤 Usuario:${NC} $USER | ${GREEN}${BOLD}💻 Hostname:${NC} $host " | cut -c -${cols}
-echo -e "    _______|@|_________  •  ==${GREEN}${BOLD}=====================================================${NC}" | cut -c -${cols}
-echo -e "   --------------------- ˚  ||${GREEN}${BOLD}🔧 SISTEMA OPERATIVO:${NC} $os" | cut -c -${cols}
-echo -e "  ||  POLICE ---- BOX  ||   ||${GREEN}${BOLD}💾 CPU:${NC} $cpu" | cut -c -${cols}
-echo -e "  -----------------------C  ||${GREEN}${BOLD}🧠 RAM:${NC} $ram" | cut -c -${cols}
-echo -e "  |  ______  |  ______  |É  ||${GREEN}${BOLD}📊 PROCESOS:${NC} $processes" | cut -c -${cols}
-echo -e "  |  |####|  |  |####|  |S  ||${GREEN}${BOLD}🖥️ KERNEL:${NC} $kernel ${GREEN}${BOLD} ${NC}"| cut -c -${cols}
-echo -e "  |  |####|  |  |####|  |A  ||${GREEN}${BOLD}⏱️ TIEMPO DE ACTIVIDAD:${NC} $uptime" | cut -c -${cols}
-echo -e "  |  |####|  |  |####|  |R  ||${GREEN}${BOLD}📅 FECHA:${NC} $(date +"%d/%m/%Y") ${GREEN}${BOLD}| HORA:${NC} $(date +"%H:%M:%S")" | cut -c -${cols}
-echo -e "% |  ------  |  ------  |˚  ||${GREEN}${BOLD}💾 USO DE CPU:${NC} $(top -bn1 | grep load | awk '{printf "%.2f%%", $(NF-2)}')" | cut -c -${cols}
-echo -e "  |  |BAD |  |  |    |  |˚  ||${GREEN}${BOLD}💽 USO DE DISCO:${NC} $disk libre de $(df -h / | awk 'NR==2{print $2}')${NC}" | cut -c -${cols}
-echo -e "  |  |WOLF|  |  |    |  |   ||${GREEN}${BOLD}=====================================================${NC}" | cut -c -${cols}
-echo -e "  |  ------  |O ------  |   ||${GREEN}${BOLD} | Arquitectura:${NC} $arch ${GREEN}${BOLD}| Versión:${NC} $kernel_version" | cut -c -${cols}
-echo -e "  |  ------  |° ------  |   ||${GREEN}${BOLD}=====================================================${NC}" | cut -c -${cols}
-echo -e "♥ |  |    |  |  |    |  |   ||${GREEN}${BOLD} ${NC}" | cut -c -${cols}
-echo -e "L |  |    |  |  |    |  |•˛ ||" | cut -c -${cols}
-echo -e "A |  ------  |  ------  |。 ||" | cut -c -${cols}
-echo -e "U |  ------  |  ------  |•  ||" | cut -c -${cols}
-echo -e "♥ |  |    |  |  |    |  |˚• ||" | cut -c -${cols}
-echo -e "˚ |  |    |  |  |    |  |•˚ ||" | cut -c -${cols}
-echo -e "  |  ------  |  ------  |   ||" | cut -c -${cols}
-echo -e " _|_____________________|_  ||" | cut -c -${cols}
-echo -e " |_______________________|  ||" | cut -c -${cols}
+echo -e " " | cut -c -${cols}
+echo -e "  。★  ˚ •    -   ˚ •。★˚˛˚   ${GREEN}${BOLD}        👤 Usuario:${NC} $USER | ${GREEN}${BOLD}💻 Hostname:${NC} $host " | cut -c -${cols}
+echo -e "      _______|@|_________  •  ${GREEN}${BOLD}  ${NC}" | cut -c -${cols}
+echo -e "     --------------------- ˚  ${GREEN}${BOLD}  🔧 SISTEMA OPERATIVO:${NC} $os" | cut -c -${cols}
+echo -e "    ||  POLICE ---- BOX  ||   ${GREEN}${BOLD}  💾 CPU:${NC} $cpu" | cut -c -${cols}
+echo -e "    -----------------------C  ${GREEN}${BOLD}  🧠 RAM:${NC} $ram" | cut -c -${cols}
+echo -e "    |  ______  |  ______  |É  ${GREEN}${BOLD}  📊 PROCESOS:${NC} $processes" | cut -c -${cols}
+echo -e "    |  |####|  |  |####|  |S  ${GREEN}${BOLD}  🖥️ KERNEL:${NC} $kernel ${GREEN}${BOLD} ${NC}"| cut -c -${cols}
+echo -e "    |  |####|  |  |####|  |A  ${GREEN}${BOLD}  ⏱️ TIEMPO DE ACTIVIDAD:${NC} $uptime" | cut -c -${cols}
+echo -e "    |  |####|  |  |####|  |R  ${GREEN}${BOLD}  📅 FECHA:${NC} $(date +"%d/%m/%Y") ${GREEN}${BOLD}| HORA:${NC} $(date +"%H:%M:%S")" | cut -c -${cols}
+echo -e "  % |  ------  |  ------  |˚  ${GREEN}${BOLD}  💾 USO DE CPU:${NC} $(top -bn1 | grep load | awk '{printf "%.2f%%", $(NF-2)}')" | cut -c -${cols}
+echo -e "    |  |BAD |  |  |    |  |˚  ${GREEN}${BOLD}  💽 USO DE DISCO:${NC} $disk libre de $(df -h / | awk 'NR==2{print $2}') ${NC}" | cut -c -${cols}
+echo -e "    |  |WOLF|  |  |    |  |   ${GREEN}${BOLD}   Arquitectura:${NC} $arch ${GREEN}${BOLD}| Versión:${NC} $kernel_version ${NC}" | cut -c -${cols}
+echo -e "    |  ------  |O ------  |   ${GREEN}${BOLD} ${NC} " | cut -c -${cols}
+echo -e "    |  ------  |° ------  |   ${GREEN}${BOLD} ${NC}" | cut -c -${cols}
+echo -e "  ♥ |  |    |  |  |    |  |   ${GREEN}${BOLD} ${NC}" | cut -c -${cols}
+echo -e "  L |  |    |  |  |    |  |•˛ " | cut -c -${cols}
+echo -e "  A |  ------  |  ------  |。 " | cut -c -${cols}
+echo -e "  U |  ------  |  ------  |•  " | cut -c -${cols}
+echo -e "  ♥ |  |    |  |  |    |  |˚• " | cut -c -${cols}
+echo -e "  ˚ |  |    |  |  |    |  |•˚ " | cut -c -${cols}
+echo -e "    |  ------  |  ------  |   " | cut -c -${cols}
+echo -e "   _|_____________________|_  " | cut -c -${cols}
+echo -e "   |_______________________|  " | cut -c -${cols}
+echo -e "  " | cut -c -${cols}
 
