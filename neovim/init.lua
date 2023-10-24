5370ff649f42c5665e9a71947de5bf3339390775
Cesar Guzman <88-9@live.com.mx>
2023-10-24 15:51:03 -0600

    clean

--- neovim/init.lua
+++ neovim/init.lua
@@ -2,12 +2,13 @@
 vim = vim
 vim.g.OSH   = os.getenv('OSH')
 vim.g.HOME  = os.getenv('HOME')
+local cmd = vim.cmd
+
 if(vim.g.OSH == nil) then
 	vim.g.OSH = vim.g.HOME.."/.bash_vim/neovim"
 else
 	vim.g.OSH = vim.g.OSH.."/neovim"
 end
-local cmd = vim.cmd
 package.path =  package.path .. ";"..vim.g.OSH.."/lua/?.lua"
 require('plugins')
 require('default')
@@ -16,13 +17,11 @@ require('appearance')
 require('principal')
 require('CocConfig')
 require('functionsKeyBindings')
+
 cmd('source '..vim.g.OSH..'/vim/appearance.vim')
 cmd('source '..vim.g.OSH..'/vim/FZF.vim')
-cmd('set laststatus=3')
 cmd('let g:loaded_ruby_provider = 0 ')
---cmd('source '..vim.g.OSH..'/vim/Functions_Java.vim')
---cmd('source '..vim.g.OSH..'/vim/Functions_C.vim')
+cmd('set laststatus=3')
 
 require('keybindings')
 vim.opt.laststatus = 3
-
